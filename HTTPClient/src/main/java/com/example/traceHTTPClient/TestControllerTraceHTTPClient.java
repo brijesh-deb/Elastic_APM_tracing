4d92815c1230188e1ee0c0036f6ff569c4c60bda
Philips <brijesh_deb@yahoo.com>
2022-08-20 18:50:23 +0530

    changes producer code

--- HTTPClient/src/main/java/com/example/traceHTTPClient/TestControllerTraceHTTPClient.java
+++ HTTPClient/src/main/java/com/example/traceHTTPClient/TestControllerTraceHTTPClient.java
@@ -2,10 +2,8 @@ package com.example.traceHTTPClient;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
--- KafkaProducer/src/main/java/com/example/traceKafkaProducer/TestControllerTraceKafkaProducer.java
+++ KafkaProducer/src/main/java/com/example/traceKafkaProducer/TestControllerTraceKafkaProducer.java
@@ -5,14 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.messaging.Processor;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.ExecutionException;
 
 @RestController
 public class TestControllerTraceKafkaProducer {
